f454ee44f936ebc749e709a1671e479495e5549e
Acho Arnold <arnold@ndolestudio.com>
2022-05-29 18:49:09 +0300

    add drawer

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer :width="400" fixed app>
+    <v-navigation-drawer v-model="drawer" fixed app>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -32,7 +32,7 @@ export default {
   data() {
     return {
       clipped: false,
-      drawer: false,
+      drawer: true,
       fixed: false,
       items: [
         {
