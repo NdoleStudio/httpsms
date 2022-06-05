1ec3af79d527d017dce342f42c125967e3ea4649
Acho Arnold <arnold@ndolestudio.com>
2022-06-05 22:33:20 +0300

    Add index for event listener log

--- api/pkg/entities/event_listener_log.go
+++ api/pkg/entities/event_listener_log.go
@@ -9,9 +9,9 @@ import (
 // EventListenerLog stores the log of all the events handled
 type EventListenerLog struct {
 	ID        uuid.UUID     `json:"id" gorm:"primaryKey;type:uuid;"`
-	EventID   string        `json:"event_id"`
+	EventID   string        `json:"event_id" gorm:"uniqueIndex:idx_event_listener_log_event_id_handler"`
 	EventType string        `json:"event_type"`
-	Handler   string        `json:"handler"`
+	Handler   string        `json:"handler" gorm:"uniqueIndex:idx_event_listener_log_event_id_handler"`
 	Duration  time.Duration `json:"duration"`
 	HandledAt time.Time     `json:"handled_at"`
 	CreatedAt time.Time     `json:"created_at"`
