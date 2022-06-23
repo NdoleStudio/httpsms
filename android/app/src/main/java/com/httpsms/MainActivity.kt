d376e17e23e1dcc5213ba63f1e869e750b6c2b96
Acho Arnold <arnold@ndolestudio.com>
2022-06-23 20:17:08 +0300

    Added sample android app

--- android/app/src/main/java/com/httpsms/MainActivity.kt
+++ android/app/src/main/java/com/httpsms/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.httpsms
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.PhoneNumberUtils
+import android.telephony.TelephonyManager
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private val logTag = MainActivity::class.simpleName ?: "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val phoneNumber = getPhoneNumber(this)
+
+        val titleText = findViewById<TextView>(R.id.cardPhoneNumber)
+        titleText.text = PhoneNumberUtils.formatNumber(phoneNumber, Locale.getDefault().country)
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getPhoneNumber(context: Context): String {
+        // Register the permissions callback, which handles the user's response to the
+        // system permissions dialog. Save the return value, an instance of
+        // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+        // or a late init var in your onAttach() or onCreate() method.
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    val toast = Toast.makeText(context, "Granted", Toast.LENGTH_SHORT)
+                    toast.show()
+                } else {
+                    val toast = Toast.makeText(context, "NOT Granted", Toast.LENGTH_LONG)
+                    toast.show()
+                }
+            }
+
+        val telephonyManager = this.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_SMS
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_NUMBERS
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // You can directly ask for the permission.
+            // The registered ActivityResultCallback gets the result of this request.
+            requestPermissionLauncher.launch(Manifest.permission.READ_SMS)
+            requestPermissionLauncher.launch(Manifest.permission.READ_PHONE_NUMBERS)
+            requestPermissionLauncher.launch(Manifest.permission.READ_PHONE_STATE)
+        }
+
+        val phoneNumber = telephonyManager.line1Number  ?: "NO_PHONE_NUMBER"
+        Log.d(logTag, phoneNumber)
+        return phoneNumber
     }
 }
