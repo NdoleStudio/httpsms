4959665fd96949e41f52bb574de97e1d57987441
Acho Arnold <arnold@ndolestudio.com>
2022-06-26 22:58:23 +0300

    Fix authdomain

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -48,7 +48,7 @@ export default {
       {
         config: {
           apiKey: 'AIzaSyClL8AX2H_F77_n8yu5FgLzBmJTiSM0NsQ',
-          authDomain: 'httpsms.com',
+          authDomain: 'httpsms-86c51.firebaseapp.com',
           projectId: 'httpsms-86c51',
           storageBucket: 'httpsms-86c51.appspot.com',
           messagingSenderId: '877524083399',
