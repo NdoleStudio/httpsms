a324956ef8134d549b53e71800354a0eb1bb8e36
Acho Arnold <arnold@ndolestudio.com>
2022-07-10 23:27:00 +0300

    Fix messages per minute

--- api/pkg/entities/phone.go
+++ api/pkg/entities/phone.go
@@ -12,7 +12,7 @@ type Phone struct {
 	UserID            UserID    `json:"user_id" example:"WB7DRDWrJZRGbYrv2CKGkqbzvqdC"`
 	FcmToken          *string   `json:"fcm_token" example:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzd....."`
 	PhoneNumber       string    `json:"phone_number" example:"+18005550199"`
-	MessagesPerMinute uint      `json:"messages_per_second" example:"1"`
+	MessagesPerMinute uint      `json:"messages_per_minute" example:"1"`
 	CreatedAt         time.Time `json:"created_at" example:"2022-06-05T14:26:02.302718+03:00"`
 	UpdatedAt         time.Time `json:"updated_at" example:"2022-06-05T14:26:10.303278+03:00"`
 }
