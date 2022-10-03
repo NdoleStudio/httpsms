98dc5fe762a9c1130d3670c749defa639be75e04
Acho Arnold <arnold@ndolestudio.com>
2022-10-03 21:18:09 +0300

    Fix annotation for API documentation

--- api/pkg/responses/billing_responses.go
+++ api/pkg/responses/billing_responses.go
@@ -5,11 +5,11 @@ import "github.com/NdoleStudio/httpsms/pkg/entities"
 // BillingUsagesResponse is the payload containing []entities.BillingUsage
 type BillingUsagesResponse struct {
 	response
-	Data []entities.Heartbeat `json:"data"`
+	Data []entities.BillingUsage `json:"data"`
 }
 
 // BillingUsageResponse is the payload containing entities.BillingUsage
 type BillingUsageResponse struct {
 	response
-	Data entities.Heartbeat `json:"data"`
+	Data entities.BillingUsage `json:"data"`
 }
