6f642d52ae9a01180b66fb39b1b6b64bf8590463
Acho Arnold <arnold@ndolestudio.com>
2023-03-10 20:38:52 +0200

    Fix refresh interval

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -39,14 +39,11 @@ export default class DefaultLayout extends Vue {
 
   mounted() {
     this.startPoller()
-    setTimeout(
-      () => {
-        if (this.poller) {
-          clearInterval(this.poller)
-        }
-      },
-      60 * 1000 * 60,
-    )
+    setTimeout(() => {
+      if (this.poller) {
+        clearInterval(this.poller)
+      }
+    }, 60 * 1000 * 60)
   }
 
   beforeDestroy(): void {
@@ -64,7 +61,7 @@ export default class DefaultLayout extends Vue {
         setAuthHeader((await this.$fire.auth.currentUser?.getIdToken()) ?? '')
         promises.push(
           this.$store.dispatch('loadThreads'),
-          this.$store.dispatch('getHeartbeat'),
+          this.$store.dispatch('getHeartbeat')
         )
       }
 
@@ -76,8 +73,8 @@ export default class DefaultLayout extends Vue {
         promises.push(
           this.$store.dispatch(
             'loadThreadMessages',
-            this.$store.getters.getThread.id,
-          ),
+            this.$store.getters.getThread.id
+          )
         )
       }
       await Promise.all(promises)
