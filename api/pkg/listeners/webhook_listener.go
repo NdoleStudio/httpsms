bae83ae802a52fe9ace28cc225f9e7e4bb61ac2b
Acho Arnold <arnold@ndolestudio.com>
2023-07-27 01:31:17 +0300

    Add support for more webhook events

--- api/pkg/listeners/webhook_listener.go
+++ api/pkg/listeners/webhook_listener.go
@@ -31,7 +31,11 @@ func NewWebhookListener(
 	}
 
 	return l, map[string]events.EventListener{
-		events.EventTypeMessagePhoneReceived: l.OnMessagePhoneReceived,
+		events.EventTypeMessagePhoneReceived:  l.OnMessagePhoneReceived,
+		events.EventTypeMessageSendExpired:    l.OnMessageSendExpired,
+		events.EventTypeMessagePhoneDelivered: l.OnMessagePhoneDelivered,
+		events.EventTypeMessageSendFailed:     l.OnMessageSendFailed,
+		events.EventTypeMessagePhoneSent:      l.OnMessagePhoneSent,
 	}
 }
 
@@ -41,6 +45,82 @@ func (listener *WebhookListener) OnMessagePhoneReceived(ctx context.Context, eve
 	defer span.End()
 
 	var payload events.MessagePhoneReceivedPayload
+	if err := event.DataAs(&payload); err != nil {
+		msg := fmt.Sprintf("cannot decode [%s] into [%T]", event.Data(), payload)
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	if err := listener.service.Send(ctx, payload.UserID, event, payload.Owner); err != nil {
+		msg := fmt.Sprintf("cannot process [%s] event with ID [%s]", event.Type(), event.ID())
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	return nil
+}
+
+// OnMessageSendExpired handles the events.EventTypeMessageSendExpired event
+func (listener *WebhookListener) OnMessageSendExpired(ctx context.Context, event cloudevents.Event) error {
+	ctx, span := listener.tracer.Start(ctx)
+	defer span.End()
+
+	var payload events.MessageSendExpiredPayload
+	if err := event.DataAs(&payload); err != nil {
+		msg := fmt.Sprintf("cannot decode [%s] into [%T]", event.Data(), payload)
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	if err := listener.service.Send(ctx, payload.UserID, event, payload.Owner); err != nil {
+		msg := fmt.Sprintf("cannot process [%s] event with ID [%s]", event.Type(), event.ID())
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	return nil
+}
+
+// OnMessageSendFailed handles the events.EventTypeMessageSendFailed event
+func (listener *WebhookListener) OnMessageSendFailed(ctx context.Context, event cloudevents.Event) error {
+	ctx, span := listener.tracer.Start(ctx)
+	defer span.End()
+
+	var payload events.MessageSendFailedPayload
+	if err := event.DataAs(&payload); err != nil {
+		msg := fmt.Sprintf("cannot decode [%s] into [%T]", event.Data(), payload)
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	if err := listener.service.Send(ctx, payload.UserID, event, payload.Owner); err != nil {
+		msg := fmt.Sprintf("cannot process [%s] event with ID [%s]", event.Type(), event.ID())
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	return nil
+}
+
+// OnMessagePhoneSent handles the events.EventTypeMessagePhoneSent event
+func (listener *WebhookListener) OnMessagePhoneSent(ctx context.Context, event cloudevents.Event) error {
+	ctx, span := listener.tracer.Start(ctx)
+	defer span.End()
+
+	var payload events.MessagePhoneSentPayload
+	if err := event.DataAs(&payload); err != nil {
+		msg := fmt.Sprintf("cannot decode [%s] into [%T]", event.Data(), payload)
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	if err := listener.service.Send(ctx, payload.UserID, event, payload.Owner); err != nil {
+		msg := fmt.Sprintf("cannot process [%s] event with ID [%s]", event.Type(), event.ID())
+		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
+	}
+
+	return nil
+}
+
+// OnMessagePhoneDelivered handles the events.EventTypeMessagePhoneDelivered event
+func (listener *WebhookListener) OnMessagePhoneDelivered(ctx context.Context, event cloudevents.Event) error {
+	ctx, span := listener.tracer.Start(ctx)
+	defer span.End()
+
+	var payload events.MessagePhoneDeliveredPayload
 	if err := event.DataAs(&payload); err != nil {
 		msg := fmt.Sprintf("cannot decode [%s] into [%T]", event.Data(), payload)
 		return listener.tracer.WrapErrorSpan(span, stacktrace.Propagate(err, msg))
