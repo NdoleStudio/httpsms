3fc8a7ddc7cbdc8616fc834726bab9e2293156c4
Acho Arnold <arnold@ndolestudio.com>
2023-10-04 09:51:58 +0300

    Drop unsed indexes

--- api/pkg/entities/message_thread.go
+++ api/pkg/entities/message_thread.go
@@ -19,7 +19,7 @@ type MessageThread struct {
 	LastMessageID      uuid.UUID     `json:"last_message_id" example:"32343a19-da5e-4b1b-a767-3298a73703ca"`
 	CreatedAt          time.Time     `json:"created_at" example:"2022-06-05T14:26:09.527976+03:00"`
 	UpdatedAt          time.Time     `json:"updated_at" example:"2022-06-05T14:26:09.527976+03:00"`
-	OrderTimestamp     time.Time     `json:"order_timestamp" gorm:"index:idx_message_threads_order_timestamp" example:"2022-06-05T14:26:09.527976+03:00"`
+	OrderTimestamp     time.Time     `json:"order_timestamp" example:"2022-06-05T14:26:09.527976+03:00"`
 }
 
 // Update a message thread after a message event
