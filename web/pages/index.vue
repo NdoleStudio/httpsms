bd14d3ed340914575f161eb4e3d8110c7927dbc6
Acho Arnold <arnold@ndolestudio.com>
2023-11-23 22:05:11 +0200

    Update counters

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -50,8 +50,8 @@
             </v-btn>
           </div>
           <p class="text-body-2 mt-2">
-            ⚡Trusted by <b>2,079+</b> happy users who have sent or received
-            more than <b>366,000+</b> messages.
+            ⚡Trusted by <b>2,208+</b> happy users who have sent or received
+            more than <b>401,881+</b> messages.
           </p>
           <div
             class="mt-4"
