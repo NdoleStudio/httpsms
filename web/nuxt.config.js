d5bc9b1b690244a4df4bb642ee2aa72a0ca741f2
Acho Arnold <arnold@ndolestudio.com>
2025-02-13 22:06:01 +0200

    Fix nuxt settings

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -68,7 +68,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/filters.ts',
+    '~/plugins/filters.ts',
     { src: '~/plugins/vue-glow', ssr: false },
     { src: '~/plugins/chart', ssr: false },
   ],
