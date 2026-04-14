f85abf629e2eb0813cc5e196b13b3b47a4ff2722
Acho Arnold <arnold@ndolestudio.com>
2026-04-14 10:29:37 +0300

    Fix display of the login page

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.1.0" apply false
-    id("com.android.library") version "9.1.0" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
 }
 
 tasks.register<Delete>("clean") {
