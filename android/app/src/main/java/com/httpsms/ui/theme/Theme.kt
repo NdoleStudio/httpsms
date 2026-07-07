4c72697ea430557229f97a4500cc0fc01235a712
Acho Arnold Ewin <arnold@ndolestudio.com>
2026-07-07 22:02:53 +0300

    fix(theme): correct status bar icon color logic

--- android/app/src/main/java/com/httpsms/ui/theme/Theme.kt
+++ android/app/src/main/java/com/httpsms/ui/theme/Theme.kt
@@ -62,7 +62,7 @@ fun HttpSmsTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
