4c74c42205cf4e369d892af01b6139636635fa44
Acho Arnold Ewin <arnold@ndolestudio.com>
2026-07-09 09:29:10 +0300

    chore: upgrade target/compile SDK to 37 and update all dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,15 +5,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.gms:google-services:4.5.0")
     }
 }
 
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
 
 tasks.register<Delete>("clean") {
